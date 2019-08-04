aff2d4d3cddad6f4b1c385c2d7c840897e76fcfe
= <=>
2019-08-04 11:34:43 -0700

    Added pooling and connection test parameters, as closed connections are creating OperationalErrors

--- accessmapapi/__init__.py
+++ accessmapapi/__init__.py
@@ -40,6 +40,7 @@ def create_app():
         SECRET_KEY=os.getenv("SECRET_KEY"),
         SQLALCHEMY_DATABASE_URI=os.getenv("SQLALCHEMY_DATABASE_URI", DEFAULTS["SQLALCHEMY_DATABASE_URI"]),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_ENGINE_OPTIONS={"pool_size": 5, "pool_pre_ping": True},
         OAUTH_CACHE_DIR=os.getenv("OAUTH_CACHE_DIR", DEFAULTS["OAUTH_CACHE_DIR"]),
         JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY"),
         JWT_IDENTITY_CLAIM="sub",
